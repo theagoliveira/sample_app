1c0da9c98127111fd39a81ae36519ab03de01cc9
Thiago Cavalcante <thiago.kun@gmail.com>
2022-01-23 04:05:08 -0300

    Add sign up page
    Changes:
    - Create a new controller for Users (+ helper, css, 'new' action and
      view, and test file)
    - Change the user_new_path to signup_path in the routes file
    - Add signup_path to home page
    - Add more tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,13 +7,24 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
 
     assert_template "static_pages/home"
+    assert_select "title", full_title
 
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+
+    get help_path
+    assert_select "title", full_title("Help")
+
+    get about_path
+    assert_select "title", full_title("About")
 
     get contact_path
     assert_select "title", full_title("Contact")
+
+    get signup_path
+    assert_select "title", full_title("Sign Up")
   end
 end
