fd0e3fe478ca025e6e3529f50a2321f37435237f
Thiago Cavalcante <thiago.kun@gmail.com>
2022-01-24 01:12:38 -0300

    Add validation to users
    Changes:
    - Validate name presence and length
    - Validate email presence, length, format (with regex) and uniqueness
    - Uniqueness is validated at the app level (with the validates option)
      and at the db level (with an unique index on the emails)
    - Downcase the email before saving
    - Remove user fixtures
    - New tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_014422) do
+ActiveRecord::Schema.define(version: 2022_01_24_024857) do
 
 # Could not dump table "sqlite_stat1" because of following StandardError
 #   Unknown type '' for column 'tbl'
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_01_24_014422) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
